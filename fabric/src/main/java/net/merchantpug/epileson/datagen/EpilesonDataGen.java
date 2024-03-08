d4bc0cae0b491781e9134a44a00839441b3b9ae2
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-03-08 22:29:54 +1100

    Add advancements.

--- fabric/src/main/java/net/merchantpug/epileson/datagen/EpilesonDataGen.java
+++ fabric/src/main/java/net/merchantpug/epileson/datagen/EpilesonDataGen.java
@@ -3,22 +3,79 @@ package net.merchantpug.epileson.datagen;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricAdvancementProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.merchantpug.epileson.Epileson;
 import net.merchantpug.epileson.registry.EpilesonItems;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementHolder;
+import net.minecraft.advancements.AdvancementType;
+import net.minecraft.advancements.Criterion;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.data.advancements.packs.VanillaAdventureAdvancements;
+import net.minecraft.data.advancements.packs.VanillaTheEndAdvancements;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
+
+import java.util.function.Consumer;
 
 public class EpilesonDataGen implements DataGeneratorEntrypoint {
 
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(EpilesonAdvancementProvider::new);
         pack.addProvider(EpilesonRecipeProvider::new);
+    }
+
+    public static class EpilesonAdvancementProvider extends FabricAdvancementProvider {
+
+        protected EpilesonAdvancementProvider(FabricDataOutput output) {
+            super(output);
+        }
+
+        @Override
+        public void generateAdvancement(Consumer<AdvancementHolder> consumer) {
+            consumer.accept(new Advancement.Builder()
+                    .parent(new ResourceLocation("end/find_end_city"))
+                    .addCriterion("epileson_sheet",
+                            InventoryChangeTrigger.TriggerInstance.hasItems(
+                                    EpilesonItems.EPILESON_SHEET))
+                    .display(EpilesonItems.EPILESON_SHEET,
+                            Component.translatable("advancements.epileson.end.obtain_epileson_sheet.title"),
+                            Component.translatable("advancements.epileson.end.obtain_epileson_sheet.description"),
+                            null,
+                            AdvancementType.TASK,
+                            true,
+                            true,
+                            false)
+                    .build(Epileson.asResource("end/obtain_epileson_sheet")));
+            consumer.accept(new Advancement.Builder()
+                    .parent(Epileson.asResource("end/obtain_epileson_sheet"))
+                    .addCriterion("epileson_armor",
+                    InventoryChangeTrigger.TriggerInstance.hasItems(
+                            EpilesonItems.EPILESON_HELMET,
+                            EpilesonItems.EPILESON_CHESTPLATE,
+                            EpilesonItems.EPILESON_LEGGINGS,
+                            EpilesonItems.EPILESON_BOOTS))
+                    .display(EpilesonItems.EPILESON_CHESTPLATE,
+                            Component.translatable("advancements.epileson.end.epileson_armor.title"),
+                            Component.translatable("advancements.epileson.end.epileson_armor.description"),
+                            null,
+                            AdvancementType.CHALLENGE,
+                            true,
+                            true,
+                            false)
+                    .build(Epileson.asResource("end/epileson_armor")));
+        }
     }
 
     public static class EpilesonRecipeProvider extends FabricRecipeProvider {
